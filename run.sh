d30878b2c469d72998becf0b51a2808f4aef722d
nemosho <shota.nemoto@gmail.com>
2019-07-01 22:53:25 +0900

    Fix result to be stored in directory

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+rm -rf iso639-1-files
+
 cat iso639-1-alpha-2.txt | while read line
 do
 	cp -r base_file $line
@@ -7,3 +9,6 @@ do
 	mv base.csv $line
 	cd ..
 done
+
+mkdir -p iso639-1-files
+ls | grep -e "^..$" | xargs -I% mv % iso639-1-files
